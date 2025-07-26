e4872ae683787ee8acc02b7b0ae85654332a9b04
Hamza1610 <muhammadhamza162003@gmail.com>
2025-07-26 14:38:32 +0100

    refactor: restructure App component to include network configuration and query client setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,39 @@ import { WalletProvider } from "@/components/WalletProvider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-const App = () => (
-  <WalletProvider>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </WalletProvider>
-);
+import { createNetworkConfig, SuiClientProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default App;
+import '@mysten/dapp-kit/dist/index.css';
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+	testnet: { url: getFullnodeUrl('testnet') },
+	mainnet: { url: getFullnodeUrl('mainnet') },
+});
+const queryClient = new QueryClient();
+
+function App() {
+	return (
+		<QueryClientProvider client={queryClient} >
+			<SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+				<WalletProvider>
+					<TooltipProvider>
+       					<Toaster />
+						<Sonner />
+						<BrowserRouter>
+							<Routes>
+								<Route path="/" element={<Index />} />
+								{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+								<Route path="*" element={<NotFound />} />
+							</Routes>
+						</BrowserRouter>
+					</TooltipProvider>
+				</WalletProvider>
+			</SuiClientProvider>
+		</QueryClientProvider>
+	);
+}
+
+export default App
